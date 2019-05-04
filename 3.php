9503b65a4c71736d72cd707ecb9b0f039d6f1e69
Alexander Zemskov <zaismit@gmail.com>
2019-05-04 16:00:20 +0300

    Create 3.php

---
+++ 3.php
@@ -0,0 +1,25 @@
+<?php
+class Friends
+{
+	function add()
+	{
+
+	}
+
+	function delete()
+	{
+
+	}
+
+	function hello()
+	{
+		echo "hello my friend";
+	}
+
+}
+$alex = new Friends();
+
+$alex -> hello();
+
+
+?>
